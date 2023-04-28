55b8bfc9f4055e1c6beadefb7dcb94329ecf5e55
Johan-web-developer <johan.flore.13@gmail.com>
2023-04-28 16:07:46 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const input = document.querySelector('#searchBar');
+input.addEventListener('change', listas);
+
+function listas(){
+    const input2 = document.querySelector('#searchBar');
+    fetch(` http://www.omdbapi.com/?s=${input2.value}&page=1&apikey=6e90f515`)
+    .then(respuesta=>{
+        console.log(respuesta);
+        return respuesta.json()
+    })
+    .then(datos=>{
+        console.log(datos.Search);
+        showHTML(datos.Search)
+    })
+}
+
+function showHTML(datos){
+    limpiar();
+    datos.forEach(pelicula=>{
+        const {Title, Year, Poster}=pelicula
+        const card = document.createElement('div')
+        card.classList.add(`estilo`)
+        card.innerHTML=`
+        <div class="card">
+        <img src="${Poster}"
+        <p class="titulo" >${Title}</p>
+        <p class="año" >${Year}</p>
+        <button>Movie</button>
+        </div>
+        `
+        document.querySelector('#container').appendChild(card)
+    });
+}
+
+
+
+function limpiar() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.remove());
+  }
+
